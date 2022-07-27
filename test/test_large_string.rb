97bd539fe023c97b20111e29e3d8da9e8b5acf9c
Tetsuro Yamazaki <yamatetsu.5629@gmail.com>
2022-07-28 02:01:06 +0900

    update test/test_large_string.rb because it exceeds message size limit

--- test/test_large_string.rb
+++ test/test_large_string.rb
@@ -17,9 +17,18 @@ class TestLargeString < Minitest::Test
     end
 
     def test_large_string
-        (2..6).collect{ |i| 10 ** i }.each do |n|
-            large_json = JSON.dump((1 .. n).collect{ |i| [i, [i]] }.to_h)
+        (2..5).collect {|i| 10 ** i }.each do |n|
+            large_json = JSON.generate((1 .. n).collect {|i| [i, [i]] }.to_h)
             assert_equal 42, Jscall.get42(large_json)
+        end
+    end
+
+    def test_too_large_string
+        (6..6).collect {|i| 10 ** i }.each do |n|
+            assert_raises(RuntimeError) do
+                large_json = JSON.generate((1 .. n).collect {|i| [i, [i]] }.to_h)
+                Jscall.get42(large_json)
+            end
         end
     end
 end
